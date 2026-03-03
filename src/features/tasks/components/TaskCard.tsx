c63e08f4050f5821180e34370f92a471f775584e
mmanueljoe <emmanuel.letsu@amalitech.com>
2026-03-03 18:36:32 +0000

    feat: create task card component with button actions

--- src/features/tasks/components/TaskCard.tsx
+++ src/features/tasks/components/TaskCard.tsx
@@ -11,16 +11,29 @@
  * 5. Handle the completed state visually (e.g., strikethrough text if completed)
  */
 
-import type { Task } from '../types'
+import type { Task } from "@/features/tasks/types";
 
 type Props = {
-  // Your code here
-}
+  task: Task;
+  onComplete: (id: number) => void;
+  onDelete: (id: number) => void;
+  onEdit: (id: number) => void;
+};
 
-export function TaskCard({ }: Props) {
+export function TaskCard({ task, onComplete, onDelete, onEdit }: Props) {
   return (
-    <div>
-      {/* Your code here */}
+    <div className="flex flex-col gap-2 p-4 bg-white border border-gray-200 rounded shadow">
+      <p className={task.completed ? "line-through opacity-50" : ""}>
+        {task.title}
+      </p>
+        {task.description && (
+            <p className="text-sm text-gray-600">{task.description}</p>
+        )}
+      <div className="flex gap-2 ">
+        <button onClick={() => onEdit(task.id)}>🖊</button>
+        <button onClick={() => onDelete(task.id)}>❌</button>
+        <button onClick={() => onComplete(task.id)}>✅</button>
+      </div>
     </div>
-  )
+  );
 }
